083fa3d2d84c724e7c4cc7e4bd43997627007702
Lucas Barroso <lucasbarroso23@gmail.com>
2021-05-20 23:35:06 -0300

    rota post carro

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 
 require("./database");
 
+app.use(express.json());
+
 app.use("/usuario", require("./src/routes/usuario.routes"));
+app.use("/carro", require("./src/routes/carro.routes"));
 
 app.listen(8000, () => {
   console.log("Servidor UP na porta 8000!");
