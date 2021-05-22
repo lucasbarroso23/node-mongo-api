9b57815340e5e18b6e872199a9c9dffcb11c10d8
Lucas Barroso <lucasbarroso23@gmail.com>
2021-05-21 23:32:15 -0300

    rota get carro e carros

--- src/routes/carro.routes.js
+++ src/routes/carro.routes.js
@@ -13,4 +13,27 @@ route.post("/", async (req, res) => {
   }
 });
 
+route.get("/", async (req, res) => {
+  try {
+    const carros = await Carro.find();
+
+    res.json({ error: false, carros });
+  } catch (error) {
+    res.json({ error: true, message: error.message });
+  }
+});
+
+route.get("/:id", async (req, res) => {
+  try {
+    const carro = await Carro.findById(req.params.id).populate(
+      "usuario_id",
+      "nome email"
+    );
+
+    res.json({ error: false, carro });
+  } catch (error) {
+    res.json({ error: true, message: error.message });
+  }
+});
+
 module.exports = route;
