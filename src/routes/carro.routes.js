90cec407c92da1e39aef3e46313f7f9d84a3a6cd
Lucas Barroso <lucasbarroso23@gmail.com>
2021-05-22 23:24:52 -0300

    rotas atualizar e deletar

--- src/routes/carro.routes.js
+++ src/routes/carro.routes.js
@@ -36,4 +36,24 @@ route.get("/:id", async (req, res) => {
   }
 });
 
+route.put("/:id", async (req, res) => {
+  try {
+    const carro = await Carro.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json({ error: false, carro });
+  } catch (error) {
+    res.json({ error: true, message: error.message });
+  }
+});
+
+route.delete("/:id", async (req, res) => {
+  try {
+    const carro = await Carro.findByIdAndDelete(req.params.id);
+    res.json({ error: false });
+  } catch (error) {
+    res.json({ error: true, message: error.message });
+  }
+});
+
 module.exports = route;
