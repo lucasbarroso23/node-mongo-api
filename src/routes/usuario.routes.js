e4189c1b7be4c3089b9e05e696ec1234408af741
Lucas Barroso <lucasbarroso23@gmail.com>
2021-05-24 09:26:13 -0300

    crud usuarios

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const Carro = require("../models/carro");
 const route = express.Router();
 
 const Usuario = require("../models/usuario");
@@ -8,6 +9,47 @@ route.post("/", async (req, res) => {
     const usuario = await new Usuario(req.body).save();
 
     return res.json({ error: false, usuario });
+  } catch (error) {
+    res.json({ error: true, message: error.message });
+  }
+});
+
+route.get("/", async (req, res) => {
+  try {
+    const usuarios = await Usuario.find();
+
+    res.json({ error: false, usuarios });
+  } catch (error) {
+    res.json({ error: true, message: error.message });
+  }
+});
+
+route.get("/:id", async (req, res) => {
+  try {
+    const usuario = await Usuario.findById(req.params.id);
+    const carros = await Carro.find({ usuario_id: req.params.id });
+
+    res.json({ error: false, usuario: { ...usuario._doc, carros } });
+  } catch (error) {
+    res.json({ error: true, message: error.message });
+  }
+});
+
+route.put("/:id", async (req, res) => {
+  try {
+    const usuario = await Usuario.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json({ error: false, usuario });
+  } catch (error) {
+    res.json({ error: true, message: error.message });
+  }
+});
+
+route.delete("/:id", async (req, res) => {
+  try {
+    const usuario = await Usuario.findByIdAndDelete(req.params.id);
+    res.json({ error: false });
   } catch (error) {
     res.json({ error: true, message: error.message });
   }
